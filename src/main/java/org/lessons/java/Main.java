a228588c502ad8229b923a9dfba79a8fecc788b3
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-05-17 15:40:52 +0200

    add: ex 1 scaffolding

---
+++ src/main/java/org/lessons/java/Main.java
@@ -0,0 +1,12 @@
+package org.lessons.java;
+
+public class Main {
+    public static void main(String[] args) {
+        Product p = new Product(90, "Apple", "Lorem ipsum dolor sit amet", 1.20, 0.22);
+        System.out.println("Original price: " + p.price + "$");
+        double taxedPrice = p.getTaxedPrice();
+        System.out.println("Price with taxes: " + taxedPrice + "$");
+        String code = p.getCode();
+        System.out.println(code);
+    }
+}
---
+++ src/main/java/org/lessons/java/Product.java
@@ -0,0 +1,42 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class Product {    
+
+    // Scanner myScanner = new Scanner(System.in);
+    int code;
+    String name;
+    String description;
+    double price;
+    double vat; //IVA, based on Italian VAT (22%)
+
+    Product(){
+        vat = (22 / 100);
+    }
+
+    Product(int code, String name, String description, double price, double vat){
+        this.code = code;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.vat = vat;
+    }
+
+    double getTaxedPrice(){
+        // double price = Double.parseDouble(myScanner.nextLine());
+        double newTaxedPrice = price + (price * vat);
+        return newTaxedPrice;
+    }
+
+    // int getCode(){
+    //     int newCode = code;
+    //     // int newCode = (int)Math.random();
+    //     return newCode;
+    // }
+
+    String getCode(){
+        String newCode = "0000" + String.valueOf(code);
+        return newCode;
+    }
+}
